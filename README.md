5c5940caa5a74a3cf13fc0e053b44dbe5a67db84
Ivan Istomin <ivan@istomin.im>
2018-07-22 03:40:06 +0300

    I drew an icon for the application

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-## The client of Retrowave Radio for Desktop
-![cassette](assets/cassette.png)
+## The Client of Retrowave Radio for Desktop
+
+
+![cassette](assets/icon_web.png)
+
+
 
 Write Radio client on Go jff. You can follow my project (in the Projects tab).
 
--- main.go
+++ main.go
@@ -4,13 +4,37 @@ import (
 	"log"
 
 	"github.com/0xAX/notificator"
+	"github.com/andlabs/ui"
 )
 
 var notify *notificator.Notificator
 
 func main() {
-	// webview.Open("Retrowave Radio for Mac",
-	// 	"https://retrowave.ru", 530, 400, true)
+	err := ui.Main(func() {
+		input := ui.NewEntry()
+		button := ui.NewButton("Greet")
+		greeting := ui.NewLabel("")
+		box := ui.NewVerticalBox()
+		box.Append(ui.NewLabel("Enter your name:"), false)
+		box.Append(input, false)
+		box.Append(button, false)
+		box.Append(greeting, false)
+		window := ui.NewWindow("Hello", 200, 100, false)
+		window.SetMargined(true)
+		window.SetChild(box)
+		button.OnClicked(func(*ui.Button) {
+			greeting.SetText("Hello, " + input.Text() + "!")
+		})
+		window.OnClosing(func(*ui.Window) bool {
+			ui.Quit()
+			return true
+		})
+		window.Show()
+	})
+	if err != nil {
+		panic(err)
+	}
+
 	notificate("https://retrowave.ru/artwork/1ffecb1c64a70d2554e9db6a259165fe3680b58e.jpg", "Dead Astronauts – Witch Hunt")
 }
 
@@ -22,7 +46,7 @@ func notificate(artworkURL string, title string) {
 
 	notify.Push("title", "text", "/home/user/icon.png", notificator.UR_CRITICAL)
 
-	// Check errors
+	// Check errorsgo run
 	err := notify.Push("error", "ops =(", "/home/user/icon.png", notificator.UR_CRITICAL)
 
 	if err != nil {
